e45e6b2791135ebd517b4cac83f1d0b2a31b567a
Frode Egeland <egeland@gmail.com>
2016-03-19 21:47:33 +0900

    WIP: move some files, get a config set up

--- src/aurelia-facebook.js
+++ src/aurelia-facebook.js
@@ -1,3 +1,12 @@
-export function configure(config) {
-  config.globalResources('./hello-world');
+import { Configure } from './configure';
+
+export function configure(aurelia, configCallback) {
+    let instance = aurelia.container.get(Configure);
+
+    // Do we have a callback function?
+    if (configCallback !== undefined && typeof(configCallback) === 'function') {
+        configCallback(instance);
+    }
+
+    aurelia.globalResources('./facebook');
 }
